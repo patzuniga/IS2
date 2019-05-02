8982a92a52e386fba605f74fe92fe38e181cdd6f
Alejandra <afernandezm@udec.cl>
2019-05-02 18:47:55 -0300

    Login bonito no funcional

--- IS2/IS2/urls.py
+++ IS2/IS2/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^', auth_views.login, {'template_name': './login/index.html'}, name="login"),
+    url(r'^login', auth_views.login, {'template_name': './login/login.html'}, name="login"),
     url(r'^logout', auth_views.logout, {'next_page': '/'}, name="logout"),
     url(r'^admin/', admin.site.urls),
     url(r'^conductor', include('apps.conductor.urls')),
