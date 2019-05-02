626f7afe522fee53a9b75f12adc18fbb407f5da6
patzuniga <50001555+patzuniga@users.noreply.github.com>
2019-05-02 15:03:39 -0400

    Update settings.py

--- IS2/IS2/settings.py
+++ IS2/IS2/settings.py
@@ -77,13 +77,9 @@ WSGI_APPLICATION = 'IS2.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Proyecto',
-        'USER': 'postgres',
-        'PASSWORD': 'basededatos1',
-        'HOST': 'localhost',
-        'PORT': 5432,
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
