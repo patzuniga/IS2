3de0bdd76ad941119bf91056df826059ee78d625
Alejandra <afernandezm@udec.cl>
2019-05-03 06:13:04 -0300

    Cambio login

--- IS2/IS2/urls.py
+++ IS2/IS2/urls.py
@@ -18,10 +18,12 @@ from django.conf.urls import url, include
 from django.conf.urls import include,url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from apps.usuario.views import my_view
 
 urlpatterns = [
-    url(r'^login', auth_views.login, {'template_name': './login/login.html'}, name="login"),
-    url(r'^logout', auth_views.logout, {'next_page': '/'}, name="logout"),
+    #url(r'^login', auth_views.login, {'template_name': './login/login.html'}, name="login"),
+    url(r'^logout',my_view),
+    url(r'^$', my_view),
     url(r'^admin/', admin.site.urls),
     url(r'^conductor', include('apps.conductor.urls')),
     url(r'^usuario', include('apps.usuario.urls')),
--- IS2/apps/usuario/views.py
+++ IS2/apps/usuario/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect, HttpResponse
 
 # Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
+
 
 def index(request):
 	return HttpResponse("tsupalo diego")
+
+def my_view(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return render(request,'login/base.html',{})
+		else:
+			return render(request, 'login/login.html',{})
+	return render(request,'login/login.html',{})
