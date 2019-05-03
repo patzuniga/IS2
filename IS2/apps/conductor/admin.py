8506f34a114234d96f0dc7c29ab64ed5981c8a2b
patzuniga <50001555+patzuniga@users.noreply.github.com>
2019-05-03 00:33:27 -0400

    Add files via upload
    cambios chofer

--- IS2/apps/conductor/admin.py
+++ IS2/apps/conductor/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import Viaje
 from .models import Usuario
+from .models import Vehiculo
 # Register your models here.
 
 admin.site.register(Usuario)
 admin.site.register(Viaje)
-
+admin.site.register(Vehiculo)
--- IS2/apps/conductor/models.py
+++ IS2/apps/conductor/models.py
@@ -3,13 +3,17 @@ from apps.usuario.models import Usuario
 from apps.viaje.models import Viaje
 # Create your models here.
 
+class Vehiculo(models.Model):
+	pantete = models.CharField(max_length=30,null=False)
+	marca = models.CharField(max_length=30,null=False)
+	modelo = models.CharField(max_length=30,null=False)
+	maleta = models.BooleanField(default=False)
 
 class Conductor(models.Model):
 	licencia = models.IntegerField()
 	clasedelicencia = models.CharField(max_length=1)
 	fecha_obtencion = models.CharField(max_length=30,null=False)
-	marca = models.CharField(max_length=30,null=False)
-	modelo = models.CharField(max_length=30,null=False)
 	user = models.OneToOneField(Usuario, null= True, blank=True, on_delete=models.CASCADE)
 	viaje = models.ForeignKey(Viaje, null= True, blank=True, on_delete=models.CASCADE)
-	
+	car = models.OneToOneField(Vehiculo, null = True, blank = True, on_delete=models.CASCADE)
+
