2902b08bde90a1aac452b3269ff222d25da337dc
Alejandra <afernandezm@udec.cl>
2019-05-23 00:25:38 -0400

    tratando de arreglar el login

--- IS2/apps/usuario/views.py
+++ IS2/apps/usuario/views.py
@@ -16,8 +16,20 @@ def my_view(request):
 		password = request.POST['password']
 		user = authenticate(request, username=username, password=password)
 		if user is not None:
-			login(request, user)
-			return render(request, 'login/base2.html',{})
+			print(request.POST)
+			if(request.POST["tipo"] == "conductor"):
+				try:
+  					user._conductor
+  					login(request, user)
+  					return render(request, 'login/base2.html',{})
+				except:
+					return render(request, 'login/login.html',{})
+
+			elif(request.POST["tipo"] == "pasajero"):
+				login(request, user)
+				return render(request, 'usuario/index.html',{})
+			else:
+				return render(request, 'login/login.html',{})
 		else:
 			return render(request, 'login/login.html',{})
 	return render(request,'login/login.html',{})
