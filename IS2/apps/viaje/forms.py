7a65939527dd7af211530b348b004cd5dfbb0baf
patzuniga <patzuniga@inf.udec.cl>
2019-06-24 15:25:02 -0400

    editar viaje obtiene distancias

--- IS2/apps/viaje/forms.py
+++ IS2/apps/viaje/forms.py
@@ -8,13 +8,13 @@ from .models import *
 class ViajeForm(forms.Form):
 
 	fecha = forms.DateField(label = "Fecha (DD/MM/YY)")
+	hora_origen = forms.TimeField(label = "Hora Origen")
 	porta_maleta = forms.BooleanField(required = False)
 	silla_niños = forms.BooleanField(required = False)
 	mascotas = forms.BooleanField(required = False)
 	tarifapreferencias = forms.IntegerField(label = "Tarifa" , min_value= 0)
 	plazas_disponibles = forms.IntegerField(label = "Plazas disponibles", min_value= 0)
 	#origen = forms.CharField(label="origen")
-	hora_origen = forms.TimeField(label = "Hora Origen")
 	#destino = forms.CharField(label="destino")
 	fecha_destino = forms.DateField(label = "Fecha (DD/MM/YY)")
 	hora_destino = forms.TimeField(label = "Hora Destino")
@@ -32,34 +32,19 @@ class ViajeForm(forms.Form):
 			raise forms.ValidationError("Las fechas indicadas no pueden ser menores a la actual")
 		elif (fecha_destino <= fecha_origen):
 			raise forms.ValidationError("Fecha y hora de término deben ser mayores a la fecha y hora de inicio.")
-
+			
 class EditarViajeForm(forms.Form):
 	origen = forms.CharField(label="origen")
 	destino = forms.CharField(label="destino")
 	fecha = forms.DateField(label = "Fecha de inicio (DD/MM/YY)")
+	hora_origen = forms.TimeField(label = "Hora Origen")
 	porta_maleta = forms.BooleanField(required = False)
 	silla_niños = forms.BooleanField(required = False)
 	mascotas = forms.BooleanField(required = False)
 	tarifapreferencias = forms.IntegerField(label = "Tarifa")
 	asientos_disponibles = forms.IntegerField(label = "Asientos disponibles")
-	hora_origen = forms.TimeField(label = "Hora Origen")
 	fecha_destino = forms.DateField(label = "Fecha de Termino (DD/MM/YY)")
 	hora_destino = forms.TimeField(label = "Hora Destino")
-
-	def clean(self):
-		cleaned_data = super().clean()
-		f1 = cleaned_data.get("fecha")
-		h1 = cleaned_data.get("hora_origen")
-		f2 = cleaned_data.get("fecha_destino")
-		h2 = cleaned_data.get("hora_destino")
-		tz = pytz.timezone('Chile/Continental')
-		actual = datetime.strptime(datetime.now(tz=tz).strftime("%d/%m/%Y %H:%M:%S") , "%d/%m/%Y %H:%M:%S")
-		fecha_origen = datetime.strptime(datetime.combine(f1,h1).strftime("%d/%m/%Y %H:%M:%S") , "%d/%m/%Y %H:%M:%S")
-		fecha_destino = datetime.strptime(datetime.combine(f2,h2).strftime("%d/%m/%Y %H:%M:%S") , "%d/%m/%Y %H:%M:%S")
-		if(actual > fecha_origen or actual > fecha_destino):
-			raise forms.ValidationError("Las fechas indicadas no pueden ser menores a la actual")
-		if (fecha_destino <= fecha_origen):
-			raise forms.ValidationError("Fecha y hora de termino deben ser mayores a la fecha y hora de inicio.")
 
 class ParadasForm(forms.Form):
 	fecha = forms.DateField(label = "Fecha de llegada")
