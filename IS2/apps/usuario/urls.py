d5876145099764c2ae1b4e1fbeebe54894d934a4
patzuniga <patzuniga@inf.udec.cl>
2019-07-08 03:09:26 -0400

    viaje transitllo

--- IS2/apps/usuario/urls.py
+++ IS2/apps/usuario/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls import include,url
 from django.contrib import admin
 
 urlpatterns = [
-	url(r'^login/$', auth_views.login, {'template_name': 'login/login.html'}, name='login'),
+	url(r'^login/$', auth_views.login, {'template_name' : 'login/login.html'}, name='login'),
 	url(r'^registro$',registro, name= 'registro'),    
 	url(r'logout$', auth_views.logout, {'next_page': '/'}, name="logout"),
 	url(r'^$', home, name='home'),
@@ -16,6 +16,7 @@ urlpatterns = [
 	url(r'^conductor/', include('apps.conductor.urls')),
 	url(r'^ver_reservas/',ver_reservas, name= 'ver_reservas'),
 	url(r'^cancelar/(?P<pk>\d+)/$',confirmacion, name = 'confirmacion'),
+	url(r'^alerta$',alerta, name= 'alerta'),    
 	url(r'^cancelar/done/(?P<pk>\d+)/$',cancelar_reserva, name= 'cancelar_reserva'),  
 	url(r'^editarperfil/$', editarperfil, name='editarperfil'),
 	url(r'^editarperfilconduct/$', editarperfilconduct, name='editarperfilconduct'),     
